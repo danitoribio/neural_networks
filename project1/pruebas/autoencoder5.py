14eeeb92e953516055b147dc774e7215fed2885e
ale9gg <alejogonzalez9@hotmail.es>
2024-03-07 18:40:48 +0100

    Funciones para automatizar todo hechas, (falta testearlas)

---
+++ project1/pruebas/autoencoder5.py
@@ -0,0 +1,170 @@
+import torch
+
+import numpy as np
+import torch
+from torch import nn
+from torch import optim
+import matplotlib.pyplot as plt
+class autoencoder5(nn.Module):
+    def __init__(self,projected_dimension, INDEX):
+        super().__init__()
+        self.projected_dimension = projected_dimension
+        self.criterion = nn.MSELoss()
+        self.learning_rate = 0.001
+        self.INDEX = INDEX
+        
+        if self.INDEX == 0:  # MNIST
+            self.name_train = f"5_layer_with_PSNR_train_MNIST_{self.projected_dimension}"
+            self.name_test = f"5_layer_with_PSNR_test_MNIST_{self.projected_dimension}"
+            self.dataset = "MNIST"
+
+        elif self.INDEX == 1:  # FMNIST
+            self.name_train = f"5_layer_with_PSNR_train_FMNIST_{self.projected_dimension}"
+            self.name_test = f"5_layer_with_PSNR_test_FMNIST_{self.projected_dimension}"
+            self.dataset = "FMNIST"
+
+        else:
+            raise ValueError("Inside autoencoder5, INDEX should be either 0 or 1.")
+
+
+        self.project = "project_1"
+        self.architecture = "linear_autoencoder"
+        self.input_image_size = 784
+        self.outputs = []
+        self.loss_during_training = []
+        self.valid_loss_during_training = []
+
+        # Building an linear encoder with Linear
+        # layer followed by Relu activation function
+        # 784 -> projected dimension
+        self.encoder = nn.Sequential(
+            nn.Linear(784, 550),
+            nn.ReLU(),
+            #nn.Dropout(p=0.2),
+            nn.Linear(550, 400),
+            nn.ReLU(),
+            #nn.Dropout(p=0.2),
+            nn.Linear(400, 300),
+            nn.ReLU(),
+            #nn.Dropout(p=0.2),
+            nn.Linear(300,200),
+            nn.ReLU(),
+            #nn.Dropout(p=0.2),
+            nn.Linear(200,projected_dimension),
+            nn.ReLU()
+        )
+
+        # Building an linear decoder with Linear
+        # layer followed by Relu activation function
+        # The Sigmoid activation function
+        # outputs the value between 0 and 1
+        # projected dimension -> 784
+        self.decoder = nn.Sequential(
+            nn.Linear(projected_dimension, 200),
+            nn.ReLU(),
+            #nn.Dropout(p=0.2),
+            nn.Linear(200, 300),
+            nn.ReLU(),
+            #nn.Dropout(p=0.2),
+            nn.Linear(300, 400),
+            nn.ReLU(),
+            #nn.Dropout(p=0.2),
+            nn.Linear(400,550),
+            nn.ReLU(),
+            #nn.Dropout(p=0.2),
+            nn.Linear(550,784),
+            nn.Tanh()
+        )
+
+        self.optimizer = optim.Adam(self.parameters(), self.learning_rate)
+
+    def forward(self, image):
+        encoded = self.encoder(image)
+        decoded = self.decoder(encoded)
+        return decoded
+
+    def psnr(self, img1, img2):  #  Peak signal-to-noise ratio (PSNR)
+        mse_criterio = nn.MSELoss()
+        mse = mse_criterio(img1, img2)
+        max_pixel = 1.0
+        psnr = 20 * torch.log10(max_pixel / torch.sqrt(mse))
+        return psnr
+
+    def do_training(self, epochs, train_data, valid_data):
+        self.train()
+
+
+        wandb.init(  # Esto es para que se guarde en la plataforma de wandb y nos evitamos tener que hacerlo manualmente
+            # Set the project where this run will be logged
+            project=self.project,
+            # We pass a run name (otherwise it’ll be randomly assigned, like sunshine-lollypop-10)
+            name=self.name_train,
+            # Track hyperparameters and run metadata
+            config={
+                "learning_rate": self.learning_rate,
+                "architecture": self.architecture,
+                "dataset": self.dataset,
+                "epochs": epochs,
+            },
+        )
+
+        # Aqui es donde empieza realmente el entrenamiento
+        for epoch in range(epochs):
+            running_loss = 0.
+            for img, _ in train_data:
+                self.optimizer.zero_grad()
+                img = img.reshape(-1, self.input_image_size)
+                reconstructed = self.forward(img)
+                loss_train = self.criterion(reconstructed, img)
+
+                running_loss += loss_train.item()
+                loss_train.backward()
+                self.optimizer.step()
+
+            self.loss_during_training.append(running_loss/len(train_data))
+
+            with torch.no_grad():
+
+                # set model to evaluation mode
+                self.eval()
+
+                running_loss = 0.
+
+                for images,_ in valid_data:
+
+                    images = images.reshape(-1, self.input_image_size)
+
+                    out = self.forward(images)
+
+                    loss_valid = self.criterion(out,images)
+
+                    running_loss += loss_valid.item()
+                self.valid_loss_during_training.append(running_loss/len(valid_data))
+
+            # set model back to train mode
+            self.train()
+
+            print("Epoch: ", epoch + 1, "Loss train: ", self.loss_during_training[-1], ", Loss valid: ", self.valid_loss_during_training[-1])
+            wandb.log({"projected dimension": projected_dimensions[run], "Loss train": self.loss_during_training[-1], "Loss valid": self.valid_loss_during_training[-1]})
+            self.outputs.append((projected_dimensions[run], epoch, img, reconstructed))
+
+        wandb.finish()
+
+    def eval_performance(self,data_test):
+
+      performance = 0
+
+      # Turn off gradients for validation, saves memory and computations
+      with torch.no_grad():
+
+          # set model to evaluation mode
+          self.eval()
+
+          for images,_ in data_test:
+
+              img = images.view(-1, self.input_image_size)
+              reconstructed = self.forward(img)
+              performance = self.psnr(reconstructed, img)
+
+
+          return performance
